252908bb40e39c560ec044fc636eaf69d3a3c95f
Jack Humphries <124582858+Slendergamer33@users.noreply.github.com>
2025-01-15 13:47:48 +0000

    Add files via upload

---
+++ _next/static/chunks/pages/docs-ce6fff1af0198704.js
@@ -0,0 +1,298 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+    [172], {
+        8002: function(e, s, a) {
+            (window.__NEXT_P = window.__NEXT_P || []).push(["/docs", function() {
+                return a(297)
+            }])
+        },
+        297: function(e, s, a) {
+            "use strict";
+            a.r(s);
+            var t = a(2322),
+                r = a(4888),
+                c = a.n(r),
+                i = a(9097),
+                n = a.n(i),
+                l = a(7871),
+                o = a(6663),
+                d = a(7606);
+            let f = () => (0, t.jsxs)(o.LH, {
+                combine: !0,
+                manager: "stagger",
+                children: [(0, t.jsx)(c(), {
+                    id: "9b41076cdfcf81f",
+                    children: ".badges{display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-box-orient:horizontal;-webkit-box-direction:normal;-webkit-flex-direction:row;-moz-box-orient:horizontal;-moz-box-direction:normal;-ms-flex-direction:row;flex-direction:row;-webkit-flex-wrap:wrap;-ms-flex-wrap:wrap;flex-wrap:wrap}.badges img{display:block;margin:0 .5rem .5rem 0}@media(min-width:600px){.badges{display:inline-grid;grid-auto-flow:column;grid-template-columns:auto;grid-template-rows:auto;gap:.5rem}.badges img{margin:0}}"
+                }), (0, t.jsxs)(d.Fq, {
+                    animated: (0, o.aa)("y", 12, 0),
+                    children: [(0, t.jsxs)(o.LH, {
+                        children: [(0, t.jsx)(o.xv, {
+                            as: "h1",
+                            fixed: !0,
+                            children: "Roadmap:"
+                        }), (0, t.jsx)(o.tu, {
+                            transitions: {
+                                entering: "content"
+                            },
+                            continuous: !0
+                        })]
+                    }), (0, t.jsx)(o.LH, {
+                        children: (0, t.jsx)(o.fS, {
+                            as: "hr",
+                            animated: (0, o.aa)("scaleX", 0, 1)
+                        })
+                    }), (0, t.jsx)(o.LH, {
+                        children: (0, t.jsxs)(o.fS, {
+                            className: "badges",
+                            "data-arwes-global-block": !0,
+                            animated: (0, o.Wx)(),
+                            children: [(0, t.jsx)("a", {
+                                href: "#",
+                                target: "_blank",
+                                className: "jsx-9b41076cdfcf81f",
+                                children: (0, t.jsx)("img", {
+                                    src: "",
+                                    alt: "",
+                                    className: "jsx-9b41076cdfcf81f"
+                                })
+                            }), (0, t.jsx)("a", {
+                                href: "",
+                                target: "_blank",
+                                className: "jsx-9b41076cdfcf81f",
+                                children: (0, t.jsx)("img", {
+                                    src: "",
+                                    alt: "",
+                                    className: "jsx-9b41076cdfcf81f"
+                                })
+                            }), (0, t.jsx)("a", {
+                                href: "",
+                                className: "jsx-9b41076cdfcf81f",
+                                children: (0, t.jsx)("img", {
+                                    src: "",
+                                    alt: "",
+                                    className: "jsx-9b41076cdfcf81f"
+                                })
+                            }), (0, t.jsx)("a", {
+                                href: "",
+                                target: "_blank",
+                                className: "jsx-9b41076cdfcf81f",
+                                children: (0, t.jsx)("img", {
+                                    src: "",
+                                    alt: "",
+                                    className: "jsx-9b41076cdfcf81f"
+                                })
+                            }), (0, t.jsx)("a", {
+                                href: "",
+                                target: "_blank",
+                                className: "jsx-9b41076cdfcf81f",
+                                children: (0, t.jsx)("img", {
+                                    alt: "",
+                                    src: "",
+                                    className: "jsx-9b41076cdfcf81f"
+                                })
+                            }), (0, t.jsx)("a", {
+                                href: "",
+                                target: "_blank",
+                                className: "jsx-9b41076cdfcf81f",
+                                children: (0, t.jsx)("img", {
+                                    src: "",
+                                    alt: "",
+                                    className: "jsx-9b41076cdfcf81f"
+                                })
+                            }), (0, t.jsx)("a", {
+                                href: "",
+                                target: "_blank",
+                                className: "jsx-9b41076cdfcf81f",
+                                children: (0, t.jsx)("img", {
+                                    src: "",
+                                    alt: "",
+                                    className: "jsx-9b41076cdfcf81f"
+                                })
+                            }), (0, t.jsx)("a", {
+                                href: "",
+                                target: "_blank",
+                                className: "jsx-9b41076cdfcf81f",
+                                children: (0, t.jsx)("img", {
+                                    src: "",
+                                    alt: "",
+                                    className: "jsx-9b41076cdfcf81f"
+                                })
+                            })]
+                        })
+                    }), (0, t.jsx)(o.LH, {
+                        children: (0, t.jsxs)(o.xv, {
+                            children: ["Tracer io, a trader alert system, begins with research and team setup in the first two months, focusing on understanding market demand, studying competitors, and assembling a small team of developers, designers, and marketers. ", (0, t.jsx)("a", {
+                                href: "",
+                                target: "_blank",
+                                className: "jsx-9b41076cdfcf81f",
+                                children: ""
+                            }), "", (0, t.jsx)("a", {
+                                href: "",
+                                target: "_blank",
+                                className: "jsx-9b41076cdfcf81f",
+                                children: ""
+                            }), "", (0, t.jsx)("a", {
+                                href: "",
+                                target: "_blank",
+                                className: "jsx-9b41076cdfcf81f",
+                                children: ""
+                            }), " ", (0, t.jsx)("a", {
+                                href: "",
+                                target: "_blank",
+                                className: "jsx-9b41076cdfcf81f",
+                                children: ""
+                            }), "", (0, t.jsx)("a", {
+                                href: "",
+                                target: "_blank",
+                                className: "jsx-9b41076cdfcf81f",
+                                children: ""
+                            }), ""]
+                        })
+                    }), (0, t.jsx)(o.LH, {
+                        children: (0, t.jsx)(o.fS, {
+                            as: "blockquote",
+                            children: (0, t.jsxs)(o.xv, {
+                                children: ["Adding internface with the selected users top buys in the last 24 hours.", (0, t.jsx)("a", {
+                                    href: "",
+                                    target: "_blank",
+                                    className: "jsx-9b41076cdfcf81f",
+                                    children: ""
+                                }), ""]
+                            })
+                        })
+                    }), (0, t.jsx)(o.LH, {
+                        children: (0, t.jsx)(o.fS, {
+                            as: "blockquote",
+                            children: (0, t.jsxs)(o.xv, {
+                                children: [(0, t.jsx)("a", {
+                                    href: "",
+                                    target: "_blank",
+                                    className: "jsx-9b41076cdfcf81f",
+                                    children: "Adding tracing with up to 10 wallets"
+                                }), "", (0, t.jsx)("a", {
+                                    className: "jsx-9b41076cdfcf81f",
+                                    children: ""
+                                }), "", (0, t.jsx)("a", {
+                                    href: "",
+                                    target: "_blank",
+                                    className: "jsx-9b41076cdfcf81f",
+                                    children: ""
+                                }), "", (0, t.jsx)("br", {
+                                    className: "jsx-9b41076cdfcf81f"
+                                }), (0, t.jsx)("a", {
+                                    href: "",
+                                    target: "_blank",
+                                    className: "jsx-9b41076cdfcf81f",
+                                    children: ""
+                                }), "", (0, t.jsx)("code", {
+                                    className: "jsx-9b41076cdfcf81f",
+                                    children: ""
+                                }), "", (0, t.jsx)("a", {
+                                    href: "",
+                                    target: "_blank",
+                                    className: "jsx-9b41076cdfcf81f",
+                                    children: ""
+                                }), ""]
+                            })
+                        })
+                    }), (0, t.jsx)(o.LH, {
+                        children: (0, t.jsxs)(o.xv, {
+                            children: ["Adding a buy/sell bot", (0, t.jsx)("a", {
+                                href: "",
+                                className: "jsx-9b41076cdfcf81f",
+                                children: ""
+                            }), "", (0, t.jsx)("code", {
+                                className: "jsx-9b41076cdfcf81f",
+                                children: ""
+                            }), "", (0, t.jsx)("a", {
+                                href: "",
+                                target: "_blank",
+                                className: "jsx-9b41076cdfcf81f",
+                                children: ""
+                            }), " "]
+                        })
+                    }), (0, t.jsx)(o.LH, {
+                        children: (0, t.jsxs)(o.xv, {
+                            children: ["Adding sniping for the selected wallets that you are tracing", (0, t.jsx)("a", {
+                                href: "",
+                                className: "jsx-9b41076cdfcf81f",
+                                children: ""
+                            }), "", (0, t.jsx)("a", {
+                                href: "",
+                                className: "jsx-9b41076cdfcf81f",
+                                children: ""
+                            }), "", (0, t.jsx)("a", {
+                                href: "",
+                                className: "jsx-9b41076cdfcf81f",
+                                children: ""
+                            }), ""]
+                        })
+                    }), (0, t.jsx)(o.LH, {
+                        children: (0, t.jsx)(o.xv, {
+                            children: 'Adding intercepting wallet buys like mevbot'
+                        })
+                    }), (0, t.jsx)("nav", {
+                        style: {
+                            marginTop: "3rem",
+                            display: "flex",
+                            justifyContent: "right"
+                        },
+                        className: "jsx-9b41076cdfcf81f",
+                        children: (0, t.jsx)(o.LH, {
+                            children: (0, t.jsx)(n(), {
+                                href: "https://dexscreener.com/", target:"_blank",
+                                children: (0, t.jsxs)(d.zx, {
+                                    frame: "hexagon",
+                                    animated: [(0, o.Wx)(), (0, o.aa)("x", -12, 0)],
+                                    onHoverAnimateIcons: !0,
+                                    tabIndex: -1,
+                                    title: "Get started",
+                                    children: [(0, t.jsx)("span", {
+                                        className: "jsx-9b41076cdfcf81f",
+                                        children: "View Coin"
+                                    }), (0, t.jsx)(l.Z, {})]
+                                })
+                            })
+                        })
+                    })]
+                })]
+            });
+            s.default = f
+        },
+        7871: function(e, s, a) {
+            "use strict";
+            var t = a(2784),
+                r = a(73),
+                c = function() {
+                    return (c = Object.assign || function(e) {
+                        for (var s, a = 1, t = arguments.length; a < t; a++)
+                            for (var r in s = arguments[a]) Object.prototype.hasOwnProperty.call(s, r) && (e[r] = s[r]);
+                        return e
+                    }).apply(this, arguments)
+                },
+                i = t.forwardRef(function(e, s) {
+                    var a = c(c({}, t.useContext(r.s)), e);
+                    return t.createElement("svg", c({
+                        width: "1.5em",
+                        height: "1.5em",
+                        viewBox: "0 0 24 24",
+                        strokeWidth: 1.5,
+                        fill: "none",
+                        xmlns: "http://www.w3.org/2000/svg",
+                        color: "currentColor",
+                        ref: s
+                    }, a), t.createElement("path", {
+                        d: "M3 12h18m0 0l-8.5-8.5M21 12l-8.5 8.5",
+                        stroke: "currentColor",
+                        strokeLinecap: "round",
+                        strokeLinejoin: "round"
+                    }))
+                });
+            s.Z = i
+        }
+    },
+    function(e) {
+        e.O(0, [299, 774, 888, 179], function() {
+            return e(e.s = 8002)
+        }), _N_E = e.O()
+    }
+]);
